33d0dff87f3e5bbfb6d97f41cd6bd5658e2cc24f
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-21 20:39:42 -0600

    no print

--- FacebookDataIngestSource.py
+++ FacebookDataIngestSource.py
@@ -54,7 +54,6 @@ class FacebookDataIngestSource:
 
   def next(self):
     if self.index < len(self.post):
-        pprint(self.post[self.index])
         return {'post' : json.loads(self.post[self.index])}
         self.index = self.index + 1
     else:
